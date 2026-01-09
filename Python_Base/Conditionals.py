0b15b77b7930e987827d8b774378dfa3e64fe1f9
Muskan Gangwani <codedbymuskan@gmail.com>
2026-01-09 18:33:31 +0530

    If...Else & Elif
    This file contains basics of python conditional statements  Like - use of If else and If ladder

---
+++ Python_Base/Conditionals.py
@@ -0,0 +1,34 @@
+
+# CONDITIONALS :-
+
+
+age = int(input("Enter your age : "))
+# IF ELSE :-
+
+if(age>=18):  
+    print("You are above the age of consent \nGood for you")
+
+else :
+    print("You are below the age of consent")
+
+print("End of program")  #it is out of else 
+
+
+# IF ELIF ELSE LADDER:-
+
+if(age>=18):  
+    print("You are above the age of consent \n Good for you")
+
+elif(age<0):
+    print("You are entering an invalid age")
+
+elif(age==0):
+    print("You are entering 0 which is not a valid age")
+
+else :
+    print("You are below the age of consent")
+
+print("End of program")  
+
+
+
